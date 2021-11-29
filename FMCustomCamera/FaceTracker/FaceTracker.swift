90266363de1c4ded3a31a4efa219a9ae1dfa1d2c
Zhouheng <zhouheng@hetao101.com>
2021-11-29 15:33:58 +0800

    update face tracker
    add vision

--- FMCustomCamera/FaceTracker/FaceTracker.swift
+++ FMCustomCamera/FaceTracker/FaceTracker.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import AVFoundation
+import Vision
 
 public protocol FaceTrackerDelegate: AnyObject {
     func faceIsTracked(faceRect: CGRect, withOffsetWidth offsetWidth: CGFloat, andOffsetHeight offsetHeight: CGFloat, andDistance distance: CGFloat)
@@ -47,6 +48,14 @@ public class FaceTracker: NSObject {
         }
     }
     
+    public func startRunning() {
+        previewLayer?.session?.startRunning()
+    }
+    
+    public func stopRunning() {
+        previewLayer?.session?.stopRunning()
+    }
+    
     public func fluidUpdateInterval(interval: CGFloat, withReactionFactor factor: CGFloat) {
         if factor <= 0 || factor > 1 {
             fatalError("Error! fluidUpdateInterval factor should be between 0 and 1")
@@ -54,7 +63,7 @@ public class FaceTracker: NSObject {
         
         self.reactionFactor = factor
         self.updateInterval = interval
-        self.perform(#selector(setDistance), with: nil, afterDelay: TimeInterval(interval))
+        self.perform(#selector(setDistance), with: nil, afterDelay: TimeInterval(interval), inModes: [.common])
     }
     
     @objc func setDistance() {
@@ -79,7 +88,7 @@ public class FaceTracker: NSObject {
         
         guard let device = AVCaptureDevice.default(for: .video) else { return false }
         guard let deviceInput = try? AVCaptureDeviceInput(device: device) else { return false}
-
+                
         if session.canAddInput(deviceInput) {
             session.addInput(deviceInput)
         }
@@ -109,18 +118,48 @@ public class FaceTracker: NSObject {
         let previewLayer = AVCaptureVideoPreviewLayer(session: session)
         previewLayer.backgroundColor = UIColor.black.cgColor
         previewLayer.videoGravity = .resizeAspectFill
+        
         previewLayer.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height)
         
+        let connection = previewLayer.connection
         if UIDevice.current.orientation == .landscapeLeft {
-            previewLayer.connection?.videoOrientation = .landscapeRight
+            connection?.videoOrientation = .landscapeRight
         } else {
-            previewLayer.connection?.videoOrientation = .landscapeLeft
+            connection?.videoOrientation = .landscapeLeft
         }
-                
+        
+        connection?.videoScaleAndCropFactor = 1.0
+//        let camScaleup: CGFloat = 0.5
+//        previewLayer.setAffineTransform(CGAffineTransform(scaleX: camScaleup, y: camScaleup))
+        
         session.startRunning()
         
         // connect the front camara to the preview layer
         guard let frontCamera = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front) else { return false }
+        
+        print("zoom --- \(frontCamera.videoZoomFactor)")
+        
+        if #available(iOS 11.0, *) {
+            print("min zoom --- \(frontCamera.minAvailableVideoZoomFactor)")
+        } else {
+            // Fallback on earlier versions
+        }
+        if #available(iOS 11.0, *) {
+            print("max zoom --- \(frontCamera.maxAvailableVideoZoomFactor)")
+        } else {
+            // Fallback on earlier versions
+        }
+        
+//        do {
+//            try frontCamera.lockForConfiguration()
+//
+//            frontCamera.ramp(toVideoZoomFactor: 2.0, withRate: 10)
+//
+//            frontCamera.unlockForConfiguration()
+//        } catch {
+//            print("set zoom factor failure --- \(error)")
+//        }
+        
         previewLayer.session?.beginConfiguration()
         if let input = try? AVCaptureDeviceInput(device: frontCamera) {
             if let inputs = previewLayer.session?.inputs {
@@ -128,6 +167,7 @@ public class FaceTracker: NSObject {
                     previewLayer.session?.removeInput(oldInput)
                 }
             }
+            
             previewLayer.session?.addInput(input)
             previewLayer.session?.commitConfiguration()
         }
@@ -146,29 +186,54 @@ extension FaceTracker: AVCaptureVideoDataOutputSampleBufferDelegate {
     public func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
         // got an image
         guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        if #available(iOS 11.0, *) {
+            VisionDetectFace(in: pixelBuffer)
+        } else {
+            CIDetectorDetectFace(sampleBuffer: sampleBuffer, pixelBuffer: pixelBuffer)
+        }
+    }
+    /*
+     case up = 1 // 0th row at top,    0th column on left   - default orientation
+
+     case upMirrored = 2 // 0th row at top,    0th column on right  - horizontal flip
+
+     case down = 3 // 0th row at bottom, 0th column on right  - 180 deg rotation
+
+     case downMirrored = 4 // 0th row at bottom, 0th column on left   - vertical flip
+
+     case leftMirrored = 5 // 0th row on left,   0th column at top
+
+     case right = 6 // 0th row on right,  0th column at top    - 90 deg CW
+
+     case rightMirrored = 7 // 0th row on right,  0th column on bottom
+
+     case left = 8 // 0th row on left,   0th column at bottom - 90 deg CCW
+     */
+    private func CIDetectorDetectFace(sampleBuffer: CMSampleBuffer, pixelBuffer: CVPixelBuffer) {
         let attachments = CMCopyDictionaryOfAttachments(allocator: kCFAllocatorDefault, target: sampleBuffer, attachmentMode: kCMAttachmentMode_ShouldPropagate)
         
         let ciImage = CIImage(cvPixelBuffer: pixelBuffer, options: attachments as? [CIImageOption : Any])
+        
         let curDeviceOrientation = UIDevice.current.orientation
-        var exifOrientation: ExifOrientationType = .PHOTOS_EXIF_0ROW_TOP_0COL_LEFT
+        var exifOrientation: CGImagePropertyOrientation = .up
         switch curDeviceOrientation {
         // Device oriented vertically, home button on the top
         case .portraitUpsideDown:
-            exifOrientation = .PHOTOS_EXIF_0ROW_LEFT_0COL_BOTTOM
+            exifOrientation = .left
         // Device oriented horizontally, home button on the right
         case .landscapeLeft:
-            exifOrientation = .PHOTOS_EXIF_0ROW_BOTTOM_0COL_RIGHT
+            exifOrientation = .down
         // Device oriented horizontally, home button on the left
         case .landscapeRight:
-            exifOrientation = .PHOTOS_EXIF_0ROW_TOP_0COL_LEFT
+            exifOrientation = .up
         // Device oriented vertically, home button on the bottom
         case .portrait:
-            exifOrientation = .PHOTOS_EXIF_0ROW_RIGHT_0COL_BOTTOM
+            exifOrientation = .right
         default:
-            exifOrientation = .PHOTOS_EXIF_0ROW_RIGHT_0COL_TOP
+            exifOrientation = .right
         }
         
-        let imageOptions = [CIDetectorImageOrientation: NSNumber(integerLiteral: exifOrientation.rawValue)]
+        let imageOptions = [CIDetectorImageOrientation: NSNumber(integerLiteral: Int(exifOrientation.rawValue))]
         let features = faceDetector?.features(in: ciImage, options: imageOptions) as? [CIFaceFeature]
         
         // get the clean aperture
@@ -178,6 +243,82 @@ extension FaceTracker: AVCaptureVideoDataOutputSampleBufferDelegate {
         let clap = CMVideoFormatDescriptionGetCleanAperture(fdesc, originIsAtTopLeft: false /*originIsTopLeft == false*/)
         
         calculateFaceBoxesForFeatures(features: features, forVideoBox: clap, deviceOrientation: curDeviceOrientation)
+    }
+    
+    @available(iOS 11.0, *)
+    private func VisionDetectFace(in image: CVPixelBuffer) {
+        let faceDetectionRequest = VNDetectFaceLandmarksRequest(completionHandler: { (request: VNRequest, error: Error?) in
+            DispatchQueue.main.async {
+                if let results = request.results as? [VNFaceObservation], results.count > 0 {
+                    print("did detect \(results.count) face(s)")
+                    self.handleFaceDetectionResults(results)
+                } else {
+                    print("did not detect any face")
+                    self.delegate?.hasNoFace()
+                }
+            }
+        })
+        let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: image, orientation: .up, options: [:])
+        try? imageRequestHandler.perform([faceDetectionRequest])
+    }
+    
+    @available(iOS 11.0, *)
+    private func handleFaceDetectionResults(_ observedFaces: [VNFaceObservation]) {
+        guard let observedFace = observedFaces.first else { return }
+        
+        let originBounds = observedFace.boundingBox
+        
+        let screenW = UIScreen.main.bounds.size.width
+        let screenH = UIScreen.main.bounds.size.height
+        
+        let rectWidth = screenW * originBounds.size.width
+        let rectHeight = screenH * originBounds.size.height
+                                    
+        let boundingBoxY = screenH * (1 - originBounds.origin.y - originBounds.size.height)
+        
+        // 前置摄像头坐标转换
+        let boundingX = screenW - originBounds.origin.x * screenW - rectWidth
+        
+        let leftEye = observedFace.landmarks?.leftEye
+        let hasLeftEye = (leftEye != nil)
+
+        var lefEyePosition = leftEye?.normalizedPoints.first ?? .zero
+        lefEyePosition = CGPoint(x: lefEyePosition.x * rectWidth + boundingX, y: boundingBoxY + (1 - lefEyePosition.y) * rectHeight)
+        
+        let rightEye = observedFace.landmarks?.rightEye
+        let hasRightEye = (rightEye != nil)
+        var rightEyePosition = rightEye?.normalizedPoints.first ?? .zero
+        rightEyePosition = CGPoint(x: rightEyePosition.x * rectWidth + boundingX, y: boundingBoxY + (1 - rightEyePosition.y) * rectHeight)
+        
+        /*
+        let w = originBounds.size.width * screenW
+        let h = originBounds.size.height * screenH
+        let x = originBounds.origin.x * screenW
+//        let y = screenH - (originBounds.origin.y * screenH) - h*/
+        
+        var facePointRect = convertRect(boundingBox: originBounds, imageSize: UIScreen.main.bounds.size)
+        
+        //前置摄像头的时候 记得转换
+        facePointRect.origin.x = screenW - facePointRect.origin.x - facePointRect.size.width
+        
+        print("face rect --- \(facePointRect)")
+        print("left eye position --- \(lefEyePosition)")
+        print("right eye position --- \(rightEyePosition)")
+        calculateDistanceWith(
+            originBounds: facePointRect,
+            faceRect: facePointRect,
+            hasLeftEye: hasLeftEye,
+            leftEyePosition: lefEyePosition,
+            hasRightEye: hasRightEye,
+            hasRightEyePosition: rightEyePosition)
+    }
+    
+    private func convertRect(boundingBox: CGRect, imageSize: CGSize) -> CGRect {
+        let w = boundingBox.size.width * imageSize.width
+        let h = boundingBox.size.height * imageSize.height
+        let x = boundingBox.origin.x * imageSize.width
+        let y = imageSize.height * (1 - boundingBox.origin.y - boundingBox.size.height) //- (boundingBox.origin.y * imageSize.height) - h;
+        return CGRect(x: x, y: y, width: w, height: h)
     }
     
     // called asynchronously as the capture output is capturing sample buffers, this method asks the face detector (if on)
@@ -192,13 +333,14 @@ extension FaceTracker: AVCaptureVideoDataOutputSampleBufferDelegate {
         let isMirrored = preview.connection?.isVideoMirrored
         
         let previewBox = FaceTracker.videoPreviewBoxForGravity(gravity: gravity, frameSize: parentFrameSize, apertureSize: clap.size)
-        
+        var originBounds: CGRect = .zero
         for ff in faces {
             // find the correct position for the square layer within the previewLayer
             // the feature box originates in the bottom left of the video frame.
             // (Bottom right if mirroring is turned on)
             faceRect = ff.bounds
-            
+//            print("face bounds --- \(faceRect)")
+            originBounds = ff.bounds
             // flip preview width and height
             var temp = faceRect.size.width
             faceRect.size.width = faceRect.size.height
@@ -221,33 +363,58 @@ extension FaceTracker: AVCaptureVideoDataOutputSampleBufferDelegate {
                 faceRect = faceRect.offsetBy(dx: previewBox.origin.x, dy: previewBox.origin.y)
             }
             
-            DispatchQueue.main.async {
-                let offsetWidth = (self.faceRect.origin.x - (160 - (self.faceRect.size.width / 2)))
-                let offsetHeight = (self.faceRect.origin.y  - ( 240 - (self.faceRect.origin.y / 2 )))
-                
-                if UIDevice.current.userInterfaceIdiom == .phone {
-                    // This is the current recongized distance. See the setDistance method for usages
-                    self.distance = (800.0 - (self.faceRect.size.width + self.faceRect.size.height)) / 10.0
-                } else {
-                    let minM = min(UIScreen.main.bounds.size.height, UIScreen.main.bounds.size.width)
-                    if self.faceRect.height > minM ||
-                        (!ff.hasLeftEyePosition && ff.hasRightEyePosition) ||
-                        (ff.hasLeftEyePosition && !ff.hasRightEyePosition) {
-                        self.distance = 15.0
-                    } else {
-                        // Pref 与 Dref为参考值在手机成像的距离与离屏幕的距离，Psf为双眼的间距
-                        // dsf = (pref / psf) * dref
-                        // 1pt ≈ 0.016cm
-                        let pref = 3.5 * (UIScreen.main.bounds.size.width / 1024.0)
-                        self.distance = (pref / (abs(ff.rightEyePosition.x - ff.leftEyePosition.x) * 0.016)) * 25.0
-                    }
-                }
-                
-                self.delegate?.faceIsTracked(faceRect: self.faceRect, withOffsetWidth: offsetWidth, andOffsetHeight: offsetHeight, andDistance: self.distance)
-            }
+            calculateDistanceWith(
+                originBounds: originBounds,
+                faceRect: faceRect,
+                hasLeftEye: ff.hasLeftEyePosition,
+                leftEyePosition: ff.leftEyePosition,
+                hasRightEye: ff.hasRightEyePosition,
+                hasRightEyePosition: ff.rightEyePosition)
         }
         
         CATransaction.commit()
+    }
+    
+    private func calculateDistanceWith(originBounds: CGRect, faceRect: CGRect, hasLeftEye: Bool, leftEyePosition: CGPoint, hasRightEye: Bool, hasRightEyePosition: CGPoint) {
+        DispatchQueue.main.async {
+            let offsetWidth = (self.faceRect.origin.x - (160 - (self.faceRect.size.width / 2)))
+            let offsetHeight = (self.faceRect.origin.y  - ( 240 - (self.faceRect.origin.y / 2 )))
+            
+            if UIDevice.current.userInterfaceIdiom == .phone {
+                // This is the current recongized distance. See the setDistance method for usages
+                self.distance = (800.0 - (self.faceRect.size.width + self.faceRect.size.height)) / 10.0
+            } else {
+//                    self.distance = (2000.0 - (orignBounds.size.width + orignBounds.size.height)) / 10.0
+                let minM = min(UIScreen.main.bounds.size.height, UIScreen.main.bounds.size.width) * 3.0 / 4.0
+//                    print("face height --- \(originBounds.height)")
+//                    print("face width --- \(originBounds.width)")
+//                    print("minM --- \(minM)")
+                let faceH = CGFloat(Int(originBounds.height / 10)) * 10
+                let faceW = CGFloat(Int(originBounds.width / 10)) * 10
+//                print("face height --- \(faceH)")
+//                print("face width --- \(faceW)")
+//                if faceH > minM || faceW > minM ||
+//                    (!hasLeftEye && hasRightEye) ||
+//                    (hasRightEye && !hasLeftEye) {
+//                    self.distance = 17.0
+//                } else {
+                    // Pref 与 Dref为参考值在手机成像的距离与离屏幕的距离，Psf为双眼的间距
+                    // dsf = (pref / psf) * dref
+                    // 1pt ≈ 0.016cm
+                    let pref = 3.0 * (UIScreen.main.bounds.size.width / 1024.0)
+                    self.distance = (pref / (abs(hasRightEyePosition.x - leftEyePosition.x) * 0.016)) * 25.0
+//                }
+            }
+            var originD = floor(self.distance)
+            if originD == 24 || originD == 25 {
+                originD = 23
+            }
+            if originD == 26 || originD == 27 {
+                originD = 28
+            }
+            print("face distance --- \(floor(self.distance))")
+            self.delegate?.faceIsTracked(faceRect: self.faceRect, withOffsetWidth: offsetWidth, andOffsetHeight: offsetHeight, andDistance: originD)
+        }
     }
     
     // find where the video box is positioned within the preview layer based on the video size and gravity
